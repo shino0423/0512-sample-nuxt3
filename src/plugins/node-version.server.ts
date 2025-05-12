f5750af02585b28dcea6dacda9a0a1282f87719b
shinoda <89439549+shino0423@users.noreply.github.com>
2025-05-12 20:31:24 +0900

    Create node-version.server.ts

---
+++ src/plugins/node-version.server.ts
@@ -0,0 +1,10 @@
+```typescript
+export default defineNuxtPlugin(() => {
+  const nodeVersion = process.version; // 例: "v18.20.4"
+  return {
+    provide: {
+      nodeVersion: nodeVersion
+    }
+  };
+});
+```
